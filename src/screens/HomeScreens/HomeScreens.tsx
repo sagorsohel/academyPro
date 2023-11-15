a74d100f067f7221b1df5c59b69c2f430e259079
sagorsohel <khaman2645@gmail.com>
2023-11-15 17:42:02 +0600

    vector icons import

--- src/screens/HomeScreens/HomeScreens.tsx
+++ src/screens/HomeScreens/HomeScreens.tsx
@@ -1,19 +1,25 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
 const HomeScreens = () => {
   return (
     <View style={styles.container}>
       <Text>HomeScreens</Text>
+      <Text>
+        <Icon
+          name="up"
+          size={30}
+          color="#900"
+        />
+      </Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1
-    }
-})
-export default HomeScreens
-
+  container: {
+    flex: 1,
+  },
+});
+export default HomeScreens;
